3a1eb24ea2bdca2761b61e597026cbc71e16a5a8
robert <robgladue@hotmail.com>
2021-11-24 20:36:43 +0000

    added a terribly basic function named countLetters

---
+++ countLetters.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} === ${expected}`);
+  }
+};
+
+const countLetters = function(input) {
+letterCount = {};
+
+  for(const letter of input) {
+    // console.log(letter);
+    if (letter === "L") {
+      letterCount.l = (letterCount.l + 1) || 1;
+    }
+    if (letter === "H") {
+      letterCount.h = (letterCount.h + 1) || 1;
+    }
+  }
+  return letterCount;
+};
+
+console.log(countLetters("LHL LHL LHL"));
